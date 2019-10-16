eb7434a5f40bb8c6907d9482d421a99f908e0dcb
John Kreitlow <jkreitlow@jwplayer.com>
2019-10-16 13:59:36 -0400

    Add status info to mpeg-dash demo

--- demos/mpeg-dash/demo.js
+++ demos/mpeg-dash/demo.js
@@ -1,4 +1,14 @@
 const playerInstance = jwplayer('player').setup({
-  playlist: 'http://cdn.jwplayer.com/v2/media/1g8jjku3?sources=mpd',
-  autostart: true
+  playlist: 'http://cdn.jwplayer.com/v2/media/1g8jjku3?sources=mpd'
+});
+
+
+const provider = document.getElementById('provider');
+playerInstance.once('meta', () => {
+  provider.textContent = playerInstance.getProvider().name;
+});
+
+const sourceType = document.getElementById('source-type');
+playerInstance.once('playlistItem', e => {
+  sourceType.textContent = e.item.sources[0].type;
 });
