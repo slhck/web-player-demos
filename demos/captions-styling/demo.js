9846666b95537f2b748e19741fee8b798502873c
John Kreitlow <jkreitlow@jwplayer.com>
2019-10-22 14:59:41 -0400

    Clean up Captions Styling demo

--- demos/captions-styling/demo.js
+++ demos/captions-styling/demo.js
@@ -1,17 +1,34 @@
-var styleObject= {};
-
-function update(style, value) {
-	console.log(style +", "+ value)
-	styleObject[style] = value;
-    jwplayer().setCaptions(styleObject);
-}
-
-jwplayer().on('ready', function(event){
-jwplayer().play(true);
+const playerInstance = jwplayer('player').setup({
+	aspectratio: '16:9',
+	autostart: true,
+	muted: true,
+	playlist: 'https://cdn.jwplayer.com/v2/media/1g8jjku3',
+	width: '100%'
 });
 
-jwplayer().on('firstFrame', function(event){
-jwplayer().seek(1);
-jwplayer().setCurrentCaptions(1)
-jwplayer().pause(true);
+const configValues = document.getElementById('config-values');
+
+function setCaptions(config) {
+	const captionsConfig = playerInstance.getCaptions();
+	const values = Object.assign({}, captionsConfig, config);
+	playerInstance.setCaptions(values);
+
+	configValues.textContent = `jwplayer().setCaptions(${JSON.stringify(values, null, 2)});`;
+	if (window.hljs) {
+		window.hljs.highlightBlock(configValues);
+	}
+}
+
+playerInstance.on('firstFrame', function() {
+	playerInstance.seek(1);
+	playerInstance.setCurrentCaptions(1);
+	playerInstance.pause(true);
+	setCaptions();
+});
+
+const form = document.getElementById('captions-styling-form');
+[...form.elements].forEach(el => {
+	el.addEventListener('change', ({ target: { id, value } }) => {
+		setCaptions({ [id]: value });
+	});
 });
