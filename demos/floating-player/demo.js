b71336505874d90bed621a743c9ac956763c8b12
John Kreitlow <jkreitlow@jwplayer.com>
2019-10-22 15:01:53 -0400

    Switch to not-broken playlist

--- demos/floating-player/demo.js
+++ demos/floating-player/demo.js
@@ -1,5 +1,5 @@
 jwplayer('player').setup({
-  playlist: 'https://cdn.jwplayer.com/v2/media/vM7nH0Kl?sources=mp4',
+  playlist: 'https://cdn.jwplayer.com/v2/media/0FDAGB12',
   autostart: true,
   floating: true,
   width: '640px',
