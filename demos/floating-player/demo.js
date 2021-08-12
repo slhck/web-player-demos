e7c2ddedd5dfe115b988d0df7bd59e11b12785b8
Monica Parra <monicaparra@MacBook-Pro.local>
2021-08-12 13:36:45 -0400

    fixing demo

--- demos/floating-player/demo.js
+++ demos/floating-player/demo.js
@@ -1,5 +1,5 @@
 jwplayer('player').setup({
-  playlist: 'https://cdn.jwplayer.com/v2/media/0FDAGB12',
+  playlist: 'https://cdn.jwplayer.com/v2/media/8L4m9FJB',
   autostart: true,
   floating: true,
   width: '640px',
